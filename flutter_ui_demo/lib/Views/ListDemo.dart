3d69e7f07b1a00de4ad4559ac08efaccb9c0f06b
CarlXu1008 <460520681@qq.com>
2019-07-16 02:46:37 +0800

    List

---
+++ flutter_ui_demo/lib/Views/ListDemo.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ListDemo extends StatefulWidget {
+  @override
+  _ListDemoState createState() => _ListDemoState();
+}
+
+class _ListDemoState extends State<ListDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ListDemo'),
+      ),
+    );
+  }
+}
